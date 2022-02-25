f7a948b254dab6d40c271551f3d808160a99d0da
Sabarish2001 <76895003+Sabarish2001@users.noreply.github.com>
2022-02-25 16:05:47 +0530

    Delete count_dna_bases.py

--- count_dna_bases.py
+++
@@ -1,9 +0,0 @@
-def count_bases(seq):
-    base_dict = {'A':0,'T':0,'G':0,'C':0}
-    for bases in seq:
-        if bases in base_dict:
-            base_dict[bases] += 1
-    return base_dict
-
-result = count_bases(sample_DNA)
-result
