688314a20199239eb0717b0e7342d2c99908dbed
Sabarish2001 <76895003+Sabarish2001@users.noreply.github.com>
2022-03-01 19:15:33 +0530

    Create bioinfofile_formats.py

---
+++ bioinfofile_formats.py
@@ -0,0 +1,7 @@
+with open("sequence.fasta","r") as f:
+    file_handle = f.read()
+    if not file_handle.startswith(">"):
+        print("NOT A FASTA FILE")
+    else:
+        print(file_handle)
+    
