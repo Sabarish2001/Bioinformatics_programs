2db531a4be9f41eb534059db35a263a2f1608a68
Sabarish2001 <76895003+Sabarish2001@users.noreply.github.com>
2022-03-11 20:13:08 +0530

    Update file_formats.py

--- file_formats.py
+++ file_formats.py
@@ -8,27 +8,25 @@ with open("sequence.fasta","r") as f:
 
 #Program to read FASTQ file
 
-def readFASTQ(filename, mode):
+def readFASTQ(filename,mode):
+    
+    sequences = []
+    qual_scores = []
+    
+    with open(filename,mode) as FASTQ:
+        while True:
+            FASTQ.readline()
+            seq = FASTQ.readline().rstrip()
+            FASTQ.readline()
+            qualities = FASTQ.readline().rstrip()
+            
+            if len(seq) == 0 and len(qualities) == 0:
+                break
+            sequences.append(seq)
+            qual_scores.append(qualities)
+    return sequences,qual_scores
 
-sequence = []
-qual_scores = []
-
-with open(filename,mode) as FASTQ:
-while True:
-FASTQ.readline() 
-seq = FASTQ.readline() 
-FASTQ.readline() 
-qualities = FASTQ.readline() 
-if len(seq) ==  0:
-break
-
-sequence.append(seq) 
-qual_scores.append(qualities) 
-
-return sequence, qual_scores
-
-
-file = input() 
-mode = input() 
-seqs,quals = readFASTQ(file, mode) 
-seqs[:5]
+file = input()
+modee = input()
+seq,qual = readFASTQ(file,modee)
+seq[:5]
